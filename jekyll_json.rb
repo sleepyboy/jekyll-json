0081a782c0d8385b0ba7db5276cb4643cb33998e
Andriy Prokopenko <webjedy@gmail.com>
2014-02-14 02:40:04 -0500

    Optimize code (remove loading config from file, replace instance variables with local ones)

--- jekyll_json.rb
+++ jekyll_json.rb
@@ -4,19 +4,19 @@ module Jekyll
     class JSONTag < Liquid::Tag
         def initialize(tag_name, text, tokens)
             @key = text.strip
-            @hash_to_jsonify = Jekyll.configuration({})[@key]
             super
         end
         def render(context)
-            @data = context['page'][@key]
+            data = context['page'][@key]
+            hash_to_jsonify = context.registers[:site].config[@key]
             # this isn't a sophisticated merge. if there are nested
             # hashes, it'll completely overwrite them (so if you have
             # deeply nested settings in _config.yml that aren't replicated
             # in your page's settings, they'll get blatted).
-            @data.each_pair do |k,v|
-                @hash_to_jsonify[k] = v
+            data.each_pair do |k,v|
+                hash_to_jsonify[k] = v
             end
-            return @hash_to_jsonify.to_json
+            return hash_to_jsonify.to_json
         end
     end
 end
